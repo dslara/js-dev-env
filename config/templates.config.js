1e0b2e29b5089d9a5191f53b113578d992d20fcd
Daniel Salvador Lara <dslara@hotmail.com>
2018-07-20 17:35:50 +0200

    add handlebars-loader

--- config/templates.config.js
+++ config/templates.config.js
@@ -1,6 +1,14 @@
 
 import path from 'path'
+import fs from 'fs'
 import PATHS from './paths.config'
+
+const getPartialsPath = (basePath = `${PATHS.SRC}/modules`) => {
+  const files = fs.readdirSync(basePath)
+  const folderFilter = (item) => !path.extname(item)
+  const foldersFirstLevel = files.filter(folderFilter)
+  return foldersFirstLevel.map(folder => path.resolve(__dirname, `../${basePath}/${folder}`))
+}
 
 const templates = () => ({
 
@@ -10,14 +18,12 @@ const templates = () => ({
         test: /\.hbs$/,
         use: [
           {
-            loader: 'html-loader'
-          },
-          {
-            loader: path.resolve(__dirname, './assemble.config.js'),
-            options: {
-              layouts: path.resolve(__dirname, `../${PATHS.DOCS}/**/*.hbs`),
-              partials: path.resolve(__dirname, `../${PATHS.SRC}/modules/**/*.hbs`),
-              data: path.resolve(__dirname, `../${PATHS.SRC}/modules/**/*.json`)
+            loader: 'handlebars-loader',
+            query: {
+              partialDirs: [].concat(
+                getPartialsPath(),
+                getPartialsPath(`${PATHS.SRC}/modules/component`)
+              )
             }
           }
         ]
